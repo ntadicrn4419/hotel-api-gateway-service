d0b8d36d53f53a10a5a5ec94a4d23cfacb3b99a3
Nikola Tadic <ntadic4419rn@raf.edu.rs>
2022-01-14 15:16:57 +0100

    Initial commit

---
+++ src/test/java/com/sk/hotelapigatewayservice/HotelApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.hotelapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
